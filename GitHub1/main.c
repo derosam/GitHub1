eea28216370482fa5fbd9798545aa294405e3995
David Rosas <drosas2010@gmail.com>
2022-01-27 13:51:17 -0500

    Initial Commit

---
+++ GitHub1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  GitHub1
+//
+//  Created by david on 27/01/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
